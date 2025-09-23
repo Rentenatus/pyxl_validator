94449195e364e9aa7beb5a3d1fb75503558e9692
JaRe <jrentenatus@outlook.de>
2025-09-23 20:08:07 +0200

    toml

--- pyxl_validator/__init__.py
+++ pyxl_validator/__init__.py
@@ -1,15 +1,25 @@
 """
 __init__.py
 
+pyxl_validator – Type-safe, semantically extensible Excel validator.
+
 <copyright>
 Copyright (c) 2025, Janusch Rentenatus. This program and the accompanying materials are made available under the
 terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/epl-v20.html
 </copyright>
 
+This package provides a modular architecture for processing and validating
+Excel files (.xls, .xlsx, .ods) with a focus on:
+- Type safety
+- Semantic enums
+- Sophisticated error classification
+- Sustainable data migration
+
+Developed for use in insurance-related, public, and collaborative contexts.
 """
 
-
+__doc__ = "pyxl_validator – Type-safe, semantically extensible Excel validator."
 __version__ = "0.1.0"
 __author__ = "Janusch Rentenatus"
 __license__ = "Apache-2.0"
