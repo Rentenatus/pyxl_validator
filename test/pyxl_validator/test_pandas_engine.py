a171e3e03072a0250f4069246b64fb6eed3f7c2c
JaRe <jrentenatus@outlook.de>
2025-10-15 11:40:31 +0200

    Review TableEnginePandas: extend columns

--- test/pyxl_validator/test_pandas_engine.py
+++ test/pyxl_validator/test_pandas_engine.py
@@ -19,69 +19,78 @@ class TestTableEnginePandas(TestCase):
             [{"bold": False}, {"italic": False}, {"underline": False}],
             [{"bold": False}, {"italic": False}, {"underline": False}]
         ])
-        self.pandas_engine = get_pandas_engine(self.df, self.fmt)
+        self.pandas_engine1 = get_pandas_engine(self.df, self.fmt)
+        self.pandas_engine2 = get_pandas_engine(self.df, self.fmt)
+        self.pandas_engine2.set_cell_value(2, 5, "Changed")
+
 
     def test_get_max_row(self):
-        self.assertEqual(self.pandas_engine.get_max_row(), 3)
+        self.assertEqual(self.pandas_engine1.get_max_row(), 3)
+        self.assertEqual(self.pandas_engine2.get_max_row(), 3)
 
     def test_get_max_col(self):
-        self.assertEqual(self.pandas_engine.get_max_col(), 3)
+        self.assertEqual(self.pandas_engine1.get_max_col(), 3)
+        self.assertEqual(self.pandas_engine2.get_max_col(), 5)
 
     def test_get_cell_value(self):
-        self.assertEqual(self.pandas_engine.get_cell_value(1, 1), "Alice")
+        self.assertEqual(self.pandas_engine1.get_cell_value(1, 1), "Alice")
+        self.assertEqual(self.pandas_engine2.get_cell_value(2, 1), "Bob")
+        self.assertEqual(self.pandas_engine2.get_cell_value(2, 5), "Changed")
 
     def test_get_row_values(self):
-        self.assertEqual(self.pandas_engine.get_row_values(2), ["Bob", 25, False])
+        self.assertEqual(self.pandas_engine1.get_row_values(2), ["Bob", 25, False])
+        self.assertEqual(self.pandas_engine2.get_row_values(2), ["Bob", 25, False, None, "Changed"])
+        print(self.pandas_engine2.get_dataframe())
 
     def test_get_cell_format(self):
-        fmt = self.pandas_engine.get_cell_format(1, 1)
+        fmt = self.pandas_engine1.get_cell_format(1, 1)
         self.assertIsInstance(fmt, dict)
         self.assertIn("bold", fmt)
         self.assertFalse(fmt["bold"])
 
     def test_get_row_formats(self):
-        formats = self.pandas_engine.get_row_formats(1)
+        formats = self.pandas_engine1.get_row_formats(1)
         self.assertEqual(len(formats), 3)
         self.assertEqual(formats[0]["bold"], False)
 
     def test_is_readonly(self):
-        self.assertFalse(self.pandas_engine.is_readonly())
+        self.assertFalse(self.pandas_engine1.is_readonly())
 
     def test_is_engine_readonly(self):
-        self.assertFalse(self.pandas_engine.is_engine_readonly())
+        self.assertFalse(self.pandas_engine1.is_engine_readonly())
 
     def test_set_cell_value(self):
-        self.pandas_engine.set_cell_value(2, 2, 35)
-        self.assertEqual(self.pandas_engine.get_cell_value(2, 2), 35)
+        self.pandas_engine1.set_cell_value(2, 2, 35)
+        self.assertEqual(self.pandas_engine1.get_cell_value(2, 2), 35)
 
     def test_add_row(self):
-        initial_rows = self.pandas_engine.get_max_row()
-        self.pandas_engine.add_row(initial_rows + 1)
-        self.assertEqual(self.pandas_engine.get_max_row(), initial_rows + 1)
+        initial_rows = self.pandas_engine1.get_max_row()
+        self.pandas_engine1.add_row(initial_rows + 1)
+        self.assertEqual(self.pandas_engine1.get_max_row(), initial_rows + 1)
 
     def test_set_row_values(self):
-        self.pandas_engine.set_row_values(2, ["Alice", 31, True])
-        self.assertEqual(self.pandas_engine.get_row_values(2), ["Alice", 31, True])
+        self.pandas_engine1.set_row_values(2, ["Alice", 31, True])
+        self.assertEqual(self.pandas_engine1.get_row_values(2), ["Alice", 31, True])
 
     def test_set_cell_format(self):
-        self.pandas_engine.set_cell_format(1, 1, {"bold": True})
-        fmt = self.pandas_engine.get_cell_format(1, 1)
+        self.pandas_engine1.set_cell_format(1, 1, {"bold": True})
+        fmt = self.pandas_engine1.get_cell_format(1, 1)
         self.assertTrue(fmt["bold"])
 
     def test_set_row_formats(self):
         new_formats = [{"bold": True}, {"italic": True}, {"underline": True}]
-        self.pandas_engine.set_row_formats(1, new_formats)
-        fmt_row = self.pandas_engine.get_row_formats(1)
+        self.pandas_engine1.set_row_formats(1, new_formats)
+        fmt_row = self.pandas_engine1.get_row_formats(1)
         self.assertEqual(fmt_row, new_formats)
 
     def test_save(self):
         try:
-            self.pandas_engine.save_as("test/tmp/test_output_engine_pandas")
+            self.pandas_engine1.save_as("test/tmp/test_output_engine_pandas")
         except Exception as e:
             self.fail(f"Saving raised an exception: {e}")
         pandas_engine2=load_pandas_engine("test/tmp/test_output_engine_pandas")
         pandas_engine2.save_as("test/tmp/test_output_engine_pandas2")
-        self.assertEqual(pandas_engine2.get_row_values(1), self.pandas_engine.get_row_values(1))
-        self.assertEqual(pandas_engine2.get_row_values(2), self.pandas_engine.get_row_values(2))
-        self.assertEqual(pandas_engine2.get_cell_format(1,1), self.pandas_engine.get_cell_format(1,1))
-        self.assertEqual(pandas_engine2.get_cell_format(2,3), self.pandas_engine.get_cell_format(2,3))
+        self.assertEqual(pandas_engine2.get_row_values(1), self.pandas_engine1.get_row_values(1))
+        self.assertEqual(pandas_engine2.get_row_values(2), self.pandas_engine1.get_row_values(2))
+        self.assertEqual(pandas_engine2.get_cell_format(1,1), self.pandas_engine1.get_cell_format(1, 1))
+        self.assertEqual(pandas_engine2.get_cell_format(2,3), self.pandas_engine1.get_cell_format(2, 3))
